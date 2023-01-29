93e718201c9de324ab2bca3796c6ef6f6560b3a8
MikeM911 <80363062+MikeM911@users.noreply.github.com>
2023-01-29 15:37:01 -0500

    added firebase files

---
+++ machine_learning/firebase.py
@@ -0,0 +1,15 @@
+
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+#Fetch the service account key JSON file contents
+cred = credentials.Certificate("serviceAccountKey.json")
+
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://smartyblinds-default-rtdb.firebaseio.com/'
+})
+
+ref = db.reference('Values/')
+data = ref.get()
+print(data)
